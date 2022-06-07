c0efbface8d4f77f7a77cc766edb53a6e289037a
Suh Lee <80612671+Suh2suh@users.noreply.github.com>
2022-06-07 18:19:59 +0900

    changed terrain
    changed terrain

---
+++ Assets/Scripts/NPCActiver.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCActiver : MonoBehaviour
+{
+
+	//현재 퀘스트 번호 체크 -> 퀘스트 번호별 필요 NPC 스폰 위치와 플레이어 위치 차이 계산 -> 가까워지면 ACTIVE
+
+}
---
+++ Assets/Scripts/TestScripts/CharacterMove.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour
+{
+	float moveSpd = 5.0f;
+	float startSpd = 15.0f;
+	float rotateSpd = 8.0f;
+	float xRotate = 0f;
+	float yRotate = 0f;
+
+	void Start()
+	{
+
+	}
+
+	void Update()
+	{
+		if (Input.GetKey(KeyCode.W))
+			gameObject.transform.Translate(Vector3.forward * moveSpd * Time.deltaTime);
+		if (Input.GetKey(KeyCode.S))
+			gameObject.transform.Translate(Vector3.back * moveSpd * Time.deltaTime);
+		if (Input.GetKey(KeyCode.A))
+			gameObject.transform.Translate(Vector3.left * moveSpd * Time.deltaTime);
+		if (Input.GetKey(KeyCode.D))
+			gameObject.transform.Translate(Vector3.right * moveSpd * Time.deltaTime);
+
+
+		if (Input.GetMouseButton(0))
+		{
+			float yRotateSize = Input.GetAxis("Mouse X") * rotateSpd;
+			yRotate = transform.eulerAngles.y + yRotateSize;
+		}
+
+
+		if (Input.GetMouseButton(1))
+		{
+			float xRotateSize = -Input.GetAxis("Mouse Y") * rotateSpd;
+			xRotate = Mathf.Clamp(xRotate + xRotateSize, -45, 80);     //상하 회전각을 -45~80까지 제한
+		}
+
+		transform.eulerAngles = new Vector3(xRotate, yRotate, 0);
+	}
+}
